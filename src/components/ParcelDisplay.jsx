fd86a79d4316f2a44cf9474fdf270bfce0e63921
clecardona@gmail.com <Dorival_06>
2021-04-01 15:15:22 +0200

    branching

--- src/components/ParcelDisplay.jsx
+++ src/components/ParcelDisplay.jsx
@@ -1,32 +1,29 @@
 // NPM packages
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 // Project files
 import MyJSON from "../data/orders.json";
 import Parcel from "./Parcel";
 
-
-
 export default function ParcelSearch({ id, parcel_id, user_name }) {
-  
   // State
-  const [request, setRequest] = useState(0);
-  const [query, setQuery] = useState("");
+  const [request, setRequest] = useState(0);  
   const [parcels, setParcels] = useState([]);
 
+ 
   // Constants
- //  api_url
+  //  api_url
 
   //Methods
 
   //handle the click
   function onSearch(event) {
+      
     event.preventDefault();
     // search name into JSON
     const matchedParcels = MyJSON.filter(
       (item) => item.user_name === query
     ).map((item) => (
-
       <Parcel
         key={item.id}
         user_name={item.user_name}
@@ -35,7 +32,6 @@ export default function ParcelSearch({ id, parcel_id, user_name }) {
         eta={item.eta}
         location_name={item.location_name}
       />
-      
     ));
     //change the state by setting the array of results as new state
     setParcels(matchedParcels);
@@ -44,24 +40,18 @@ export default function ParcelSearch({ id, parcel_id, user_name }) {
 
   // Return
   return (
-    
-      <div className="container-search">
-        {/* Search bar */}
-        {
-          <form onSubmit={onSearch} id="search-bar">
-            <input
-              id="search-area"
-              type="text"
-              placeholder="Enter user_name"
-              value={query}
-              //handle the data: "string" that will be searched by sending him to the state
-              onChange={(event) => setQuery(event.target.value)}
-            />
+    <div className="container-results">
+      
+      <ul>
+        <li>Number</li>
+        <li>Status</li>
+        <li>ETA</li>
+        <li>Pick up Location</li>
+      </ul>
 
-            <input id="search-button" type="submit" value="Search" />
-          </form>
-        }
-      </div>   
-  )
+      {<ol>{parcels}</ol>}
 
+      <div id="results-footer">-</div>
+    </div>
+  );
 }
