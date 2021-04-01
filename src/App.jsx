ad7001669d52106002e0b68d79e32fed516b32dc
clecardona@gmail.com <Dorival_06>
2021-04-01 19:23:56 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
+import { useEffect} from "react";
+
 import './styles/base.css';
 import Header from './components/Header'
 import ParcelSearch from './components/ParcelSearch'
 
+
 export default function App() {
+
+  // Methods 
+  useEffect(() => {
+    document.title = "Parcel Tracking"
+ }, []);
+
   return (
       <div className="App">
+       
      < Header />
-
      <ParcelSearch />
      
     </div>
