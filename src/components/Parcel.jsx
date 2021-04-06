2b0d9e611e81ad8a761bd6711c2d16c708a0f8de
clecardona@gmail.com <Dorival_06>
2021-04-06 22:16:29 +0200

    -smqll fix styling links

--- src/components/Parcel.jsx
+++ src/components/Parcel.jsx
@@ -1,37 +1,38 @@
 // NPM packages
 import Moment from "react-moment";
-import { Popover } from '@varld/popover';
+import { Popover } from "@varld/popover";
 
-
-export default function Parcel({information}) {
-// constants
-  const estimatedArrivalDate = <Moment format="DD MMM YY">{information.eta}</Moment>;
-const lat = information.location_coordinate_latitude;
-const lng= information.location_coordinate_longitude;
-const location_URL = "https://maps.google.com/?q="+lat+","+lng;
-
+export default function Parcel({ information }) {
+  // constants
+  const estimatedArrivalDate = (
+    <Moment format="DD MMM YY">{information.eta}</Moment>
+  );
+  const lat = information.location_coordinate_latitude;
+  const lng = information.location_coordinate_longitude;
+  const location_URL = "https://maps.google.com/?q=" + lat + "," + lng;
 
   return (
     <li key={information.id}>
-<Popover className="parcel-id"
+      <Popover        
         popover={({ visible, close }) => {
           return (
-            <div className="popover">              
-              <p>Sender : {information.sender} </p>                          
+            <div className="popover">
+              <p><em>Sender :</em> <br/>{information.sender} </p>
               <button onClick={close}>Close</button>
             </div>
           );
         }}
-        >
-      <button># {information.parcel_id} </button>
+      >
+        <button># {information.parcel_id} </button>
       </Popover>
-      
+
       <p>{information.status}</p>
       <p>{estimatedArrivalDate} </p>
-      <p><a href={location_URL} target="_blank" >  {information.location_name} </a> </p>
-
-      
-
+      <p className="link">
+        <a href={location_URL} target="_blank" rel="noreferrer" >          
+          {information.location_name}
+        </a>
+      </p>
     </li>
   );
 }
